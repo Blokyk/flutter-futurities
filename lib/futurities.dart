e0cfdb68f271a18dad00cfe63c0432c696fff319
blokyk <courvoisier.zoe@gmail.com>
2025-05-05 01:46:10 +0200

    fix: refresh DelayedFuture's params after hot reload
    When hot reloading a `DelayedFutureProvider`, we shouldn't keep using
    the old `create` argument value, but instead propagate it to the
    underlying `DelayedFuture` so it can be updated properly.
    
    In practice, this means that now, if you change this:
        DelayedFutureProvider(create: foo)
    To this:
        DelayedFutureProvider(create: bar)
    Then hot reload will make sure that change is propagated properly :)
    
    Obviously, it can't do magic (i.e. if the future has already started or
    completed, it won't run the new future just to get the "correct"
    result), but it shouldn't anyway -- that would be a nightmare for
    everyone, especially when unawaited async stuff is involved x_x

--- lib/futurities.dart
+++ lib/futurities.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 class DelayedFuture<T> with ChangeNotifier {
-  final Create<Future<T>> create;
+  Create<Future<T>> create;
 
   ConnectionState state;
   Future<T>? future;
@@ -21,7 +23,7 @@ class DelayedFuture<T> with ChangeNotifier {
       future = value,
       create = ((_) => value);
 
-  DelayedFuture.done({required T value})
+  DelayedFuture.done({required FutureOr<T> value})
     : state = ConnectionState.done,
       future = Future.value(value),
       create = ((_) => Future.value(value));
@@ -72,7 +74,7 @@ class DelayedFuture<T> with ChangeNotifier {
   }
 }
 
-class DelayedFutureProvider<T> extends SingleChildStatelessWidget {
+class DelayedFutureProvider<T> extends SingleChildStatefulWidget {
   final Create<Future<T>> create;
   final T initialValue;
   final T? loadingValue;
@@ -93,28 +95,58 @@ class DelayedFutureProvider<T> extends SingleChildStatelessWidget {
   });
 
   @override
+  State<DelayedFutureProvider<T>> createState() =>
+      _DelayedFutureProviderState<T>();
+}
+
+class _DelayedFutureProviderState<T>
+    extends SingleChildState<DelayedFutureProvider<T>> {
+  late DelayedFuture<T> future;
+
+  @override
+  void initState() {
+    future = DelayedFuture(create: widget.create);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(DelayedFutureProvider<T> oldWidget) {
+    // replace the DelayedFuture's [create], so that when (re)started
+    // it will use the correct one (and not just the one from the first
+    // construction, as was assigned in `initState`)
+    //
+    // note: yes, mutation is ugly; but unfortunately, in this case, we
+    //       can't just create a new copy and then mutate the [create]
+    //       method: if the DelayedFuture has already been started, the
+    //       completion callback would be called on the "old" future,
+    //       not the new/mutated one that we'd be using. this would, in
+    //       turn, prevent the new DelayedFuture's from updating and
+    //       notifying its listeners, including the UI (through both the
+    //       [ChangeNotifierProvider] and the [FutureProvider])
+    future.create = widget.create;
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget buildWithChild(BuildContext context, Widget? child) {
-    return ChangeNotifierProvider(
-      // we use the [create] version here because that'll create a separate state to store
-      // the `DelayedFuture` in, which *won't* be reset on hot reload
-      create: (_) => DelayedFuture(create: create),
+    return ChangeNotifierProvider.value(
+      value: future,
       child: child,
       builder: (context, child) {
         var delayedFuture = context.watch<DelayedFuture<T>>();
         if (delayedFuture.isSleeping) {
           // if it hasn't started yet, just send the initial value
           return Provider.value(
-            value: initialValue,
-            builder: builder,
+            value: widget.initialValue,
+            builder: widget.builder,
             child: child,
           );
         } else {
           // if we've already started, delegate the rest of the responsibility to FutureProvider
           return FutureProvider.value(
             value: delayedFuture.future,
-            initialData: loadingValue ?? initialValue,
-            catchError: catchError,
-            builder: builder,
+            initialData: widget.loadingValue ?? widget.initialValue,
+            builder: widget.builder,
             child: child,
           );
         }
@@ -127,7 +159,7 @@ class DelayedFutureBuilder<T> extends StatelessWidget {
   final DelayedFuture<T> future;
 
   final WidgetBuilder onNone, onWaiting;
-  final Widget Function(BuildContext, T) onDone;
+  final Widget Function(BuildContext context, T data) onDone;
 
   const DelayedFutureBuilder({
     super.key,
